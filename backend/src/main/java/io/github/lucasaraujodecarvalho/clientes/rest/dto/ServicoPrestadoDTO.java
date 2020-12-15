f147f7921755a4e941c945d48a8da09e74a9015e
lucas <lucas.car.araujo@gmail.com>
2020-12-15 15:27:03 -0300

    adicionando servico prestado

--- backend/src/main/java/io/github/lucasaraujodecarvalho/clientes/rest/dto/ServicoPrestadoDTO.java
+++ backend/src/main/java/io/github/lucasaraujodecarvalho/clientes/rest/dto/ServicoPrestadoDTO.java
@@ -1,2 +1,17 @@
-package io.github.lucasaraujodecarvalho.clientes.rest.dto;public class ServicoPrestadoDTO {
+package io.github.lucasaraujodecarvalho.clientes.rest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ServicoPrestadoDTO {
+
+    private String descricao;
+
+    private String preco;
+
+    private String data;
+
+    private Integer idCliente;
 }
