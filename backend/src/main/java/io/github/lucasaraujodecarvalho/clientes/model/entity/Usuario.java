e9f2b4f260369aa68eee31e0945f588945cd4b67
lucas <lucas.car.araujo@gmail.com>
2021-01-18 19:21:10 -0300

    adicionando oauth2.0 na qual faz login in memoria

--- backend/src/main/java/io/github/lucasaraujodecarvalho/clientes/model/entity/Usuario.java
+++ backend/src/main/java/io/github/lucasaraujodecarvalho/clientes/model/entity/Usuario.java
@@ -15,7 +15,7 @@ public class Usuario {
     private Integer id;
 
     @Column(unique = true, name = "login")
-    private String usernmae;
+    private String username;
 
     @Column(name = "senha")
     private String password;
