adf84ecaf951264d89dbcb80ae9730813e759cd9
lucas <lucas.car.araujo@gmail.com>
2021-03-16 15:49:44 -0300

    refatorando

--- backend/src/main/java/io/github/lucasaraujodecarvalho/clientes/service/UsuarioService.java
+++ backend/src/main/java/io/github/lucasaraujodecarvalho/clientes/service/UsuarioService.java
@@ -4,14 +4,11 @@ import io.github.lucasaraujodecarvalho.clientes.exception.UsuarioCadastradoExcep
 import io.github.lucasaraujodecarvalho.clientes.model.entity.Usuario;
 import io.github.lucasaraujodecarvalho.clientes.model.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -40,8 +37,7 @@ public class UsuarioService implements UserDetailsService {
     }
 
     public List<Usuario> obterUsuarios() {
-        List<Usuario> usuarios = usuarioRepository.findAll();
-        return usuarios;
+        return usuarioRepository.findAll();
     }
 
 }
